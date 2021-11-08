12bbd74723c99a54e0feb0b548823e4399df0d3c
Kasper <krs2609@gmail.com>
2021-11-08 21:38:27 +0100

    sum

--- src/sum.c
+++ src/sum.c
@@ -1,6 +1,19 @@
 #include "sum.h"
 
+//Function that returns the sum of all array elements with recursion
 int sum(int a[], int n)
 {
-    return 0;
+    int res; //result
+
+    //Recursion
+    if (n > 0) 
+    {
+        res = a[n-1] + sum(a, n-1);    
+    }
+    
+    else 
+    {
+        return 0;
+    }
+    return res;
 }
